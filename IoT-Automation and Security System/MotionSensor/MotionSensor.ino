d8c341db362b55679cee4556479876680d7d1ca3
Deepak Singhal <deepak.singhal1995+git@gmail.com>
2020-10-26 15:37:58 +0530

    Update MotionSensor.ino

--- IoT-Automation and Security System/MotionSensor/MotionSensor.ino
+++ IoT-Automation and Security System/MotionSensor/MotionSensor.ino
@@ -1,6 +1,14 @@
-// Always give time for PIR to warm up. A time of atleaset 50 seconds is sufficient.
-// Led is on for fixed pirDelay if there is a motion.
-int pirPin = 13;
+/*
+Always give time for PIR to warm up. A time of atleaset 50 seconds is sufficient.
+In this code inbuilt led is on for fixed pirDelay if there is a motion else off.
+Refer the Pirpinout picture in reference folder for pinout.
+PIR Connection
+PIR    ESP32
+VCC    Vin
+GND    GND
+DOUT   26 (Any digital pin and change code accordingly)
+*/
+int pirPin = 26;
 long timePir = millis();
 bool pirFlag = false;
 const int pirDelay = 5000;
@@ -9,6 +17,8 @@ void setup() {
   Serial.begin(9600);
   pinMode(2, OUTPUT);
   pinMode(pirPin, INPUT);
+  Serial.println("Warm up delay of 40 seconds");
+  delay(40000);
 }
 void loop() {
   MotionSensor();
